995e25d29ec1e71e53e09f7e6b77efd721a57bce
balaji-githubstore <balajidinakaran1@gmail.com>
2025-11-20 16:04:51 +0530

    Constructor and method overloading

---
+++ src/com/metlife/methodoverloading/CalcRunner.java
@@ -0,0 +1,11 @@
+package com.metlife.methodoverloading;
+
+public class CalcRunner {
+    public static void main(String[] args) {
+
+        Calculator obj=new Calculator();
+        obj.add( (double) 1,1);
+
+//        Math.max(1.0,1);
+    }
+}
